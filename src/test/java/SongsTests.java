dfa985a5714b989c60b0c73873146cdc00ef8f47
PetrenkoS <petrenko.s@yahoo.com>
2024-12-10 09:12:43 -0500

    Update SongsTests.java

--- src/test/java/SongsTests.java
+++ src/test/java/SongsTests.java
@@ -1,7 +1,14 @@
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SongsTests extends BaseTest {
@@ -32,4 +39,76 @@ public class SongsTests extends BaseTest {
         Assert.assertTrue(bannerMessage.contains("Deleted playlist"));
     }
 
+    @Test
+    public void playSongWithRightClick() throws InterruptedException {
+        login("planner@testpro.io", "6JooL8gp");
+        driver.findElement(By.cssSelector(".songs")).click();
+        WebElement firstSong = driver.findElement(By.cssSelector(".song-item:nth-child(1)"));
+        // right click
+        Actions actions = new Actions(driver);
+        actions.contextClick(firstSong).perform();
+        driver.findElement(By.cssSelector(".playback")).click();
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void playSongWithHoverOver() throws InterruptedException {
+        login("planner@testpro.io", "6JooL8gp");
+
+        WebElement footerLogo = driver.findElement(By.cssSelector(".player-controls"));
+        // hover over
+        Actions actions = new Actions(driver);
+        actions
+                .moveToElement(footerLogo)
+                .perform();
+        driver.findElement(By.cssSelector("[data-testid='play-btn']")).click();
+        Thread.sleep(1500);
+        Assert.assertTrue(driver.findElement(By.cssSelector("[data-testid='pause-btn']")).isDisplayed());
+    }
+
+    @Test
+    public void countSongs() throws InterruptedException {
+        login("planner@testpro.io", "6JooL8gp");
+        driver.findElement(By.cssSelector(".songs")).click();
+        List<WebElement> songs = driver.findElements(By.cssSelector(".song-item .title"));
+        int size = songs.size();
+        System.out.println(size);
+        List<String> songNames = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            String name = songs.get(i).getText();
+            songNames.add(name);
+        }
+        System.out.println(songNames);
+        Assert.assertTrue(songNames.contains("Epic Song"));
+
+    }
+
+    @Test
+    public void renamePlaylist() {
+        String playlistName = generateRandomPlaylistName();
+        login("planner@testpro.io", "6JooL8gp");
+        WebElement firstCustomPlaylist = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
+        String nameBefore = firstCustomPlaylist.getText();
+        boolean isCustomPlaylistExists = firstCustomPlaylist.isDisplayed();
+        if(!isCustomPlaylistExists){
+            createNewPlaylistWhileAddingSong(playlistName);
+        }
+        // double click
+        new Actions(driver)
+                .doubleClick(firstCustomPlaylist)
+                .perform();
+        //
+        WebElement input = driver.findElement(By.cssSelector(".playlist:nth-child(3) input"));
+        input.sendKeys(Keys.chord(Keys.COMMAND, "A", Keys.DELETE)); // this should work for Mac
+       // input.sendKeys(Keys.chord(Keys.CONTROL, "A", Keys.DELETE)); // this should work for Windows
+        input.sendKeys("1000");
+        input.sendKeys(Keys.ENTER);
+        String nameAfter = firstCustomPlaylist.getText();
+        // Using Soft Assert
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertFalse(nameBefore.equals(nameAfter),
+                "=== The song name expected to be different ===");
+        // This line is required for soft assert, otherwise a test won't fail in case of failed assertion
+        softAssert.assertAll();
+    }
 }
