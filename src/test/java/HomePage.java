dfeed36d4a23e3323cd377b2d0005abe4fe91775
mkhatibsu22 <mohamed.khatib@testpro.io>
2024-12-15 21:23:00 -0500

    homework 22

--- src/test/java/HomePage.java
+++ src/test/java/HomePage.java
@@ -12,8 +12,9 @@ public class HomePage extends BasePage
 
     public void renamePlaylist()
     {
-        action.doubleClick(findElement(By.cssSelector(".playlist.playlist:nth-of-type(4) a"))).perform();
-
+        By selectedPlaylist =By.cssSelector(".playlist.playlist:nth-of-type(4) a");
+        //action.doubleClick(findElement(By.cssSelector(".playlist.playlist:nth-of-type(4) a"))).perform();
+        doubleClick(selectedPlaylist);
         WebElement auxwe =driver.findElement(By.cssSelector(".playlist:nth-child(4) input"));
         auxwe.sendKeys(Keys.chord(Keys.CONTROL,"A",Keys.BACK_SPACE));
         auxwe.sendKeys( generateRandomPlaylistName());
