955405d21c9b5e2a03f968d6d175ca788a5c4962
mkhatibsu22 <mohamed.khatib@testpro.io>
2024-12-17 19:50:44 -0500

    hw24

--- src/test/java/Homework24.java
+++ src/test/java/Homework24.java
@@ -1,13 +1,16 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class Homework23 {
+public class Homework24 {
 
 
     LoginPage loginPage;
@@ -30,6 +33,26 @@ public class Homework23 {
              System.out.println("here212");
         loginPage1.login("mohamed.khatib@testpro.io", "ZWyxkAKX");
         homePage1.renamePlaylist();
+    }
+
+    public pickBrowser(String browser)
+    {
+        switch (browser)
+        case "firefox":
+            WebDriverManager.firefoxdriver().setup();
+            return driver=new FirefoxDriver();
+        case "MicrosoftEdge":
+            WebDriverManager.edgedriver().setup();
+        EdgeOptions edgeOptions=new EdgeOptions();
+        edgeOptions.addArguments("--remote-allow-origins=*");
+        return driver=new EdgeDriver(edgeOptions);
+
+        default:
+            WebDriverManager.chromedriver()driver().setup();
+        ChromeOptions chromeOptions =new ChromeOptions();
+        ChromeOptions.addArguments("--remote-allow-origins=*");
+        return driver=new ChromeDriver(chromeOptions);
+
     }
 
 }
