da3fd0df7356b3cbda8d41c86a7a84e284dc10b8
mkhatibsu22 <mohamed.khatib@testpro.io>
2024-12-27 20:19:36 -0500

    edited

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,52 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class MyStepdefs {
+    WebDriver driver;
+    WebDriverWait wait;
+    @Given("I am in the Login Page")
+    public void iAmInTheLoginPage() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        wait =new WebDriverWait(driver,Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+    }
+
+    @And("I enter registered email")
+    public void iEnterRegisteredEmail(String email) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type=email]"))).sendKeys(email);
+    }
+
+    @And("I enter valid password")
+    public void iEnterValidPassword(String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type=password]"))).sendKeys(password);
+    }
+
+    @When("I click submit button")
+    public void iClickSubmitButton() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type=submit]"))).click();
+    }
+
+    @Then("I must be logged in successfully")
+    public void iMustBeLoggedInSuccessfully() {
+        Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img=avatar"))).isDisplayed());
+    }
+}
