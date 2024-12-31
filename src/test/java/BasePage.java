6c814b1b26e1acb496819fff0ceabfd525189d65
mkhatibsu22 <mohamed.khatib@testpro.io>
2024-12-30 22:52:55 -0500

    edited

--- src/test/java/BasePage.java
+++ src/test/java/BasePage.java
@@ -6,11 +6,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 import java.util.List;
 import java.util.Locale;
@@ -64,6 +71,58 @@ public  class BasePage {
         action = new Actions(driver);
 
     }
+    public WebDriver pickBrowser(String browser) throws MalformedURLException
+    {
+        DesiredCapabilities caps =new DesiredCapabilities();
+        String gridURL="http://192.168.56.1:4444";
+
+        switch (browser){
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver=new FirefoxDriver();
+            case "MicrosoftEdge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions=new EdgeOptions();
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                return driver=new EdgeDriver(edgeOptions);
+            case "grid-Edge":
+                caps.setCapability("browserName","MicrosoftEdge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+
+            case "grid-firefox":
+                caps.setCapability("browserName","firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+
+            case "grid-chrome":
+                caps.setCapability("browserName","chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions =new ChromeOptions();
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                return driver=new ChromeDriver(chromeOptions);
+
+        }
+    }
+    @BeforeMethod
+    @Parameters({"BaseURL"})
+    public void lunchBrowser(String BaseURL) throws MalformedURLException
+    {
+        ThreadLocal threadLocal=new ThreadLocal<>();
+        driver=pickBrowser(System.getProperty("browser"));
+        threadLocal.set(driver);
+
+        wait=new WebDriverWait(getDriver(),Duration.ofSeconds(10));
+        action = new Actions(getDriver());
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        url=BaseURL;
+        navigateToPage();
+
+    }
+
+
+
 
 
     }
