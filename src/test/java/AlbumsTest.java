80b64f1a3464b169c833462c0a5be7fa9e6d5779
mkhatibsu22 <mohamed.khatib@testpro.io>
2025-01-29 22:22:32 -0500

    edited

--- src/test/java/AlbumsTest.java
+++ src/test/java/AlbumsTest.java
@@ -32,7 +32,25 @@ public class AlbumsTest {
         passwordField=driver.findElement(By.cssSelector("input[type='password']"));
         submitbtnField=driver.findElement(By.cssSelector("button[type='submit']"));
        login("mohamed.khatib@testpro.io", "ZWyxkAKX");
-        driver.get("https://qa.koel.app/#!/albums");
+        //driver.get("https://qa.koel.app/#!/albums");
+    WebElement plusCircleCP = driver.findElement(By.cssSelector("[data-testid='sidebar-create-playlist-btn']"));
+    plusCircleCP.click();
+    WebElement createSmartPlaylist=driver.findElement(By.cssSelector("[data-testid='playlist-context-menu-create-smart']"));
+    createSmartPlaylist.click();
+
+    WebElement formplaylistName= driver.findElement(By.cssSelector("input[name='name'][type='text']"));
+    WebElement formModelSelect= driver.findElement(By.cssSelector("select[name='model[]']"));
+    WebElement formOperatorSelect= driver.findElement(By.cssSelector("select[name='operator[]']"));
+    WebElement formInputNameValue= driver.findElement(By.cssSelector("input[name='value[]']"));
+    WebElement formAddRule= driver.findElement(By.cssSelector("button[class='btn-add-rule']"));
+    WebElement formSave= driver.findElement(By.cssSelector("button[type='submit']"));
+
+
+
+
+
+
+
 
     }
 
