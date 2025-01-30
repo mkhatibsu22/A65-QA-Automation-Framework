59e8a57cab1163854d69bc1737f1f6f640ebb76b
mkhatibsu22 <mohamed.khatib@testpro.io>
2025-01-29 23:23:51 -0500

    edited

---
+++ src/test/java/SmartPlaylist.java
@@ -0,0 +1,80 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SmartPlaylist {
+    WebDriver driver;
+    WebElement emailField;
+    WebElement passwordField;
+    WebElement submitbtnField;
+    WebDriverWait wait;
+    WebElement formPlaylistName;
+    WebElement formModelSelect;
+    WebElement formOperatorSelect;
+    WebElement formInputNameValue;
+    WebElement formAddRule;
+    WebElement formAddGroup;
+    WebElement formSave;
+
+@Test
+     void given() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
+        // options.addArguments("--start-maximized");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        System.out.println("here212");
+        emailField=driver.findElement(By.cssSelector("input[type='email']"));
+        passwordField=driver.findElement(By.cssSelector("input[type='password']"));
+        submitbtnField=driver.findElement(By.cssSelector("button[type='submit']"));
+       login("mohamed.khatib@testpro.io", "ZWyxkAKX");
+        //driver.get("https://qa.koel.app/#!/albums");
+
+    WebElement plusCircleCP = driver.findElement(By.cssSelector("#playlists .fa.fa-plus-circle.create"));
+    wait.until((ExpectedConditions.elementToBeClickable(plusCircleCP))).click();
+    WebElement createSmartPlaylist=driver.findElement(By.cssSelector("[data-testid='playlist-context-menu-create-smart']"));
+    wait.until((ExpectedConditions.elementToBeClickable(createSmartPlaylist))).click();
+
+     formPlaylistName= driver.findElement(By.cssSelector("input[name='name'][type='text']"));
+     formModelSelect= driver.findElement(By.cssSelector("select[name='model[]']"));
+      formOperatorSelect= driver.findElement(By.cssSelector("select[name='operator[]']"));
+     formInputNameValue= driver.findElement(By.cssSelector("input[name='value[]']"));
+     formAddRule= driver.findElement(By.cssSelector("button[class='btn-add-rule']"));
+     formAddGroup= driver.findElement(By.cssSelector("button[class='btn-add-group']"));
+     formSave= driver.findElement(By.cssSelector("button[type='submit']"));
+
+    formPlaylistName.sendKeys("first smart playlist");
+    //formModelSelect.sendKeys("Albums");
+
+    }
+
+
+    public  void login(String email,String password)
+    {
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+        submitbtnField.click();
+
+    }
+  //  @Test
+void createSmartPlaylistOneRule()
+{
+    formPlaylistName.sendKeys("first smart playlist");
+    //formModelSelect.sendKeys("title");
+}
+
+}
